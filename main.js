24f5d2744b740399f74bff879b5f0f1397494514
JanakiPa <76910394+JanakiPa@users.noreply.github.com>
2021-04-12 16:03:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var changeimage=[
+    "https://mail.google.com/mail/u/0?ui=2&ik=47ec981b6a&attid=0.1&permmsgid=msg-a:r5525469851323886506&th=178c79cc05d471e3&view=fimg&sz=s0-l75-ft&attbid=ANGjdJ_o9iGKoOoiRoiDqXFcum6l5Oov-du-ohV8XJcCTHmnTpPKhNsilJ5Gs30fDkMbzjSTfEY_Bjp1XRo6a2M5FCziEEfBi34o3icQKRYrrXyOim_GhIeiW5Qhnkk&disp=emb&realattid=178c79c6a27eee3b4631",
+     "https://mail.google.com/mail/u/0?ui=2&ik=47ec981b6a&attid=0.1&permmsgid=msg-f:1696864870156291554&th=178c799be9814de2&view=fimg&sz=s0-l75-ft&attbid=ANGjdJ8QP1G-Ny9d72tFx6p8HwIYi36iaLtz9z1WmqdDCvIerWBi7hO61I5l4PWboD_0ehjGUGGqrSU82TFSe4EVRX0uxfGUN_ou1qNaSFr76TUlw5Juoif78vgKpvo&disp=emb&realattid=178c79979adf7acb5291", 
+     "https://mail.google.com/mail/u/0?ui=2&ik=47ec981b6a&attid=0.2&permmsgid=msg-f:1696864870156291554&th=178c799be9814de2&view=fimg&sz=s0-l75-ft&attbid=ANGjdJ-BUnwvCAuU23_57pwNOxA7CVc4kUXb1a9hiyRrWGJSl04EA3HPOE8LezyMUoYQ1d40SSSyLvc96jZFF8j-UM4CQurn7EbesGwc0rYEq5k4lSDXznKuHaqs5Qk&disp=emb&realattid=ii_knezzxxx0"
+]
+[
+    
+    "Me", "My Mom", "My Dad"
+]
+function nextslide(){
+  
+    document.getElementById("coverimg").src= changeimage;}
